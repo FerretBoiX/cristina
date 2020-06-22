eb56413df5e811926b20ba9da9f9ca4e3af2f992
Daniel <54555281+Danielthemouse@users.noreply.github.com>
2020-06-22 15:12:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,9 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-var facts = ["מה קשור",
-             "סוקה בלאט",
-             "אידי נחוי",
-            "עדי עגבניה ואני מכבדת את זה",
-            "אתם תחוו עכשיו את הכוח האמיתי שלי! \n \n *קולות רוסיים*",
-            "עדי תעשה לי ילד",
-            "דה אני רוסי וזה טוב \n בלאט, יש לי כבד כמו של דוב \n נייט, מים זה מגעיל \n לא סתם אומרים וודקה קונקטינג פיפל",
-            "СУКА БЛЯДЬ"];
+var facts = ["אני נפתחת לכל מי שרוצה",
+             "אני הדלת וליעד הוא המפתח"
+             ];
 
 bot.on('ready', () => {
 
@@ -22,17 +17,15 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
 
-// if(message.author.id == "545999659769004060") {
-//  message.channel.send("גם אני אוהבת אותך עדי");
-//  }
-   if(message.content.startsWith("!קריס")) {
-    message.channel.send(message.content.substring(6));
-    message.delete()
+  if(message.content.startsWith("!דור")) {
+  message.channel.send(message.content.substring(5));
+  message.delete()
   }
-// else if(message.isMemberMentioned(bot.user)) {
-//  var fact = Math.floor(Math.random() * facts.length);
-//  message.channel.send(facts[fact]);
-//  }
+  else if(message.isMemberMentioned(bot.user)) {
+  var fact = Math.floor(Math.random() * facts.length);
+  message.channel.send(facts[fact]);
+  }
+  
 });
 
  
