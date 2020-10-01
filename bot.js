c584093cbad78f0a00d3c36aafee58963250e8dd
Daniel <54555281+Danielthemouse@users.noreply.github.com>
2020-10-01 09:35:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,3 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.on("listening", function() {
-  console.log("ok, server is running");
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
 const Discord = require("discord.js");
 const TOKEN = process.env.TOKEN;
 
