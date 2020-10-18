0f53e2bbf9872d4a57171dd604471398c9b751a3
Daniel <54555281+Danielthemouse@users.noreply.github.com>
2020-10-18 08:58:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,22 @@
 const Discord = require("discord.js");
 const TOKEN = process.env.BOT_TOKEN;
-
 var bot = new Discord.Client();
-var times = 1;
 
-bot.on('ready', function() {
-    // Now, you can use the message variable inside
-    console.log("it's time")
-  
-      var interval = setInterval (async function () {
-          // use the message's channel (TextChannel) to send a new message
-        //let userGet = await bot.fetchUser("359969081971179521");
-        //if(userGet.presence.status == "offline") {
-            bot.channels.get("439021446530990081").send(times).then(m => m.delete());
-            times += 1; 
-        //}
-      }, 61 * 1000);  
+
+bot.on('ready', function() { //a ברגע שהבוט נדלק
+    //a אומר אם הבוט פועל
+    console.log("it's time");
+});
+
+bot.on('message', function() { //a ברגע שהודעה נשלחת
+    
+    if(message.content.startsWith("אני")) { //a אם ההודעה מתחילה במילה אני
+        message.channel.send("היי " + message.content.substring(2) + ", אני אבא"); //a שולח משהו לחדר של ההודעה
+    }
+    
 });
 
 
 
-bot.login(TOKEN);
+bot.login(TOKEN); //a שולח את הקוד לדיסקורד
 
